62ead1a13618e6e0c50426315fede406db7b3b59
ayushtibra <aayushtibra1997@gmail.com>
2022-09-01 17:43:50 +0530

    remove imports not needed

--- pages/CustomMultiSelectDropDown.tsx
+++ pages/CustomMultiSelectDropDown.tsx
@@ -1,15 +1,4 @@
-import { useEffect, useState } from "react";
 import FormComponent from "../components/FormComponent/FormComponent";
-import MultiSelectDropdownNew from "../components/MultiSelectDropdown/MultiSelectDropdown";
-import ProgressBar from "../components/ProgressBar/ProgressBar";
-
-const options = [
-  // { value: "select all", label: "Select All" },
-  { value: "green", label: "Greens" },
-  { value: "red", label: "Red" },
-  { value: "yellow", label: "Yellow" },
-  { value: "blue", label: "Blue" },
-];
 
 const CustomMultiSelectDropDown = () => {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import FormComponent from "../components/FormComponent/FormComponent";
 
 import styles from "../styles/Home.module.css";
 
